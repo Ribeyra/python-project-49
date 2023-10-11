911b5006836bbcab61a32ac3692138527f31f9d8
ribeyra <ribeyra@gmail.com>
2023-10-11 22:58:45 +0300

    complete the third step
    create and update cli.py
    update brain_games.py
    build and publish (--dry-run) package

---
+++ brain_games/cli.py
@@ -0,0 +1,14 @@
+import prompt
+
+
+def welcome_user() -> str:
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}')
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
--- brain_games/script/brain_games.py
+++ brain_games/script/brain_games.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+from ..cli import welcome_user
 
 
 def main():
     text = 'Welcome to the Brain Games!'
     print(text)
+    welcome_user()
 
 
 if __name__ == '__main__':
